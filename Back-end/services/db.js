c6ffba19f9242d47221bd9922846d5b5f889156c
AmrAssal <amrkhaledassal2024@gmail.com>
2026-04-12 21:03:29 +0200

    Set DNS servers and force IPv4 for MongoDB
    Use the dns module to set Google DNS servers (8.8.8.8, 8.8.4.4) and update mongoose.connect with family: 4 to prefer IPv4 and serverSelectionTimeoutMS: 5000 to fail faster when selecting a server. This helps avoid IPv6/DNS resolution issues when connecting to the MongoDB instance.

--- Back-end/services/db.js
+++ Back-end/services/db.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const dns = require('dns');
 dotenv.config();
+
+dns.setServers(['8.8.8.8', '8.8.4.8']);
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            family: 4,
+            serverSelectionTimeoutMS: 5000,
+        });
         console.log("MongoDB connected successfully");
     }catch(err){
         console.log(err.message);
