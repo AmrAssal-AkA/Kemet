fd77e02f184ee893b6321ceeffe9a6e0432bca30
Menzaro <khalodzm@gmail.com>
2026-04-15 23:49:10 +0200

    Add files via upload

---
+++ front-end/tailwind.config.mjs
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'kemet-gold': '#FBBF24', 
+        'kemet-navy': '#111827', 
+        'kemet-dark-blue': '#1D3557', 
+        'kemet-gray': '#E5E7EB', 
+      },
+    },
+  },
+  plugins: [],
+};
