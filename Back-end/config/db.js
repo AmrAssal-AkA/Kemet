2a97f90ca9f2027ecfa828a6e0caca749890035b
AmrAssal <amrkhaledassal2024@gmail.com>
2026-05-06 22:35:52 +0300

    Update db.js

--- Back-end/config/db.js
+++ Back-end/config/db.js
@@ -1,18 +1,16 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const dns = require("dns");
 dotenv.config();
 
-dns.setServers(["8.8.8.8", "8.8.4.8"]);
 
-const connectDB = async (nxt) => {
+const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       family: 4,
       serverSelectionTimeoutMS: 5000,
     });
   } catch (err) {
-    nxt(err);
+    console.error("Error connecting to MongoDB:", err);
   }
 };
 
