a12f9c032f96578e3602187b29baf3b5d4d6642c
Joey-jr <yy9258886@gmail.com>
2026-05-31 01:54:50 +0300

    git add src/utils/apiBaseUrl.js git commit -m "Fix API base URL fallback handling"

--- front-end/src/utils/apiBaseUrl.js
+++ front-end/src/utils/apiBaseUrl.js
@@ -1,7 +1,11 @@
 const DEFAULT_API_BASE_URL = "http://localhost:8000";
 
 export function getApiBaseUrl() {
-  return process.env.NEXT_PUBLIC_Backend_URL ;
+  return (
+    process.env.NEXT_PUBLIC_Backend_URL ||
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    DEFAULT_API_BASE_URL
+  );
 }
 
 export function buildApiUrl(path = "") {
